b898e2a3b3fede5e1217f206a9847ff94266b21f
n1cken <nicken333@gmail.com>
2021-09-27 12:39:41 +0200

    added backend file structure

---
+++ src/backend/src/main/java/com/group4/ljudio/LjudioApplication.java
@@ -0,0 +1,13 @@
+package com.group4.ljudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LjudioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LjudioApplication.class, args);
+    }
+
+}
