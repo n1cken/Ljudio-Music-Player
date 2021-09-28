38a48c68eeb742a4ed62134212a87eaa600774ef
nicholas <nicken333@gmail.com>
2021-09-28 14:22:21 +0200

    Moved the TopBar and Router to to TopBar.jsx

--- src/App.js
+++ src/App.js
@@ -1,45 +1,15 @@
 
 import './styles/App.css';
 import Player from './components/Player';
-import Backward from './assets/svgFiles/backward-solid.svg';
-import play from './assets/svgFiles/play-solid.svg';
-import forward from './assets/svgFiles/forward-solid.svg';
+import TopBar from './components/TopBar'
 
-
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom';
-
-import StartPage from './pages/StartPage';
-import AboutPage from './pages/AboutPage';
-import ContactPage from './pages/ContactPage';
-import ProductPage from './pages/ProductPage';
 
 function App() {
   return (
-    <Router>
-      <nav class="top">
-        <Link to="/"> Start </Link>
-        <Link to="/AboutPage"> About </Link>
-        <Link to="/ContactPage"> Contact </Link>
-        <Link to="/ProductPage"> Products </Link>
-      </nav>
-
-      <main>
-        <Route path="/" exact component={StartPage} />
-        <Route path="/AboutPage" exact component={AboutPage} />
-        <Route path="/ContactPage" exact component={ContactPage} />
-        <Route path="/ProductPage" exact component={ProductPage} />
-        <Route path="/ProductPage/:videoId" component={ProductPage} />
-      </main>
-
-
-
+<div>
+        <TopBar/>
         <Player/>
-    </Router>
-
+</div>
   );
 }
 
--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,1 +1,51 @@
-let searchActivated = false;
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom';
+
+import StartPage from '../pages/StartPage';
+import AboutPage from '../pages/AboutPage';
+import ContactPage from '../pages/ContactPage';
+import ProductPage from '../pages/ProductPage';
+
+class TopBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchActivated : false
+        };
+
+    }
+
+    render() {
+        return (
+            <div>
+                <Router>
+
+                <nav className="top">
+                    <Link to="/"> Start </Link>
+                    <Link to="/AboutPage"> About </Link>
+                    <Link to="/ContactPage"> Contact </Link>
+                    <Link to="/ProductPage"> Products </Link>
+                </nav>
+
+                <main>
+                    <Route path="/" exact component={StartPage} />
+                    <Route path="/AboutPage" exact component={AboutPage} />
+                    <Route path="/ContactPage" exact component={ContactPage} />
+                    <Route path="/ProductPage" exact component={ProductPage} />
+                    <Route path="/ProductPage/:videoId" component={ProductPage} />
+                </main>
+
+                </Router>
+            </div>
+        );
+    }
+}
+
+export default TopBar
+
+
+
