c65abacec4723a1f154574199ccfeca56df545a6
adamkarmander <71024067+adamkarmander@users.noreply.github.com>
2021-10-01 14:52:17 +0200

    Made the play/pause button able to be toggled

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import Backward from "../assets/svgFiles/backward-solid.svg";
 import play from "../assets/svgFiles/play-solid.svg";
+import pause from "../assets/svgFiles/pause-solid.svg";
 import forward from "../assets/svgFiles/forward-solid.svg";
 
 import '../styles/Player.css';
+
+let isPlaying = false;
 
 class Player extends React.Component {
     constructor(props) {
         super(props);
         this.state = {};
+    }
+
+    playOrPause() {
+        if (isPlaying == false) {
+            document.getElementById("playPause").src = play;
+            isPlaying = true;
+        } else {
+            document.getElementById("playPause").src = pause;
+            isPlaying = false;
+        }
     }
 
     render() {
@@ -17,7 +30,7 @@ class Player extends React.Component {
                 <div className="bottom">
                     <div></div>
                     <img src={Backward} alt="backwards" />
-                    <img src={play} alt="play" />
+                    <img src={pause} alt="play" id="playPause" onClick={this.playOrPause} />
                     <img src={forward} alt="forward" />
                     <div></div>
                 </div>
