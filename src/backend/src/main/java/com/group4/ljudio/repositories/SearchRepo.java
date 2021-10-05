f941ca221ed6d4ed27f2b7256b9ab6aca3fdc607
sholmqvist <sholmqvist89@gmail.com>
2021-10-05 16:12:14 +0200

    Tested a few Backend modifications with Nicholas

---
+++ src/backend/src/main/java/com/group4/ljudio/repositories/SearchRepo.java
@@ -0,0 +1,9 @@
+package com.group4.ljudio.repositories;
+
+import com.group4.ljudio.entities.Search;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SearchRepo extends JpaRepository <Search, String > {
+
+
+}
