a32fe203635aae6bee705c170e2587ee34d5fbae
sholmqvist <sholmqvist89@gmail.com>
2021-10-05 12:28:07 +0200

    Implemented and updated parts for a start of a Backend

---
+++ src/backend/src/main/java/com/group4/ljudio/configs/SQLiteIdentityColumnSupport.java
@@ -0,0 +1,25 @@
+package com.group4.ljudio.configs;
+
+import org.hibernate.boot.MappingException;
+import org.hibernate.dialect.identity.IdentityColumnSupportImpl;
+
+//Class that enables us to use Autoincrement in our Springboot setup
+
+public class SQLiteIdentityColumnSupport extends IdentityColumnSupportImpl {
+
+    @Override
+    public boolean supportsIdentityColumns() {
+        return true;
+    }
+
+    @Override
+    public String getIdentitySelectString(String table, String column, int type)
+            throws MappingException {
+        return "select last_insert_rowid()";
+    }
+
+    @Override
+    public String getIdentityColumnString(int type) throws MappingException {
+        return "integer";
+    }
+}
