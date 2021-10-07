3a2a99bcbd8bb0ea210df1ba5d6917dd50b97b4a
nicholas <nicken333@gmail.com>
2021-10-07 11:30:15 +0200

    ADAM & NICHOLAS - click on song now gets videoId information

--- frontend/src/pages/PlayingPage.jsx
+++ frontend/src/pages/PlayingPage.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, {useState, useEffect}from 'react'
 import { useParams } from 'react-router-dom'
 
 function PlayingPage() {
+    const url = 'https://yt-music-api.herokuapp.com/api/yt/song/'
 
     const { videoId } = useParams()
 
-    console.log(videoId)
-
+    useEffect(() => {
+        fetch(url + videoId)
+            .then(res => res.json())
+            .then(data =>console.log(data.artist))
+    })
 
     return (
         <div>
             <h3>videoId: {videoId}</h3>
+
             <p>This page will show current song that is played</p>
         </div>
     )
