d51a3b3ac535f772faffd0fac49c60f0c1826600
dennishoang2010 <61258188+dennishoang2010@users.noreply.github.com>
2021-10-07 17:42:42 +0200

    Video player now implemented into playing page with 3 buttons

--- frontend/src/pages/PlayingPage.jsx
+++ frontend/src/pages/PlayingPage.jsx
@@ -1,37 +1,67 @@
-import React, {useState, useEffect}from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
 function PlayingPage() {
+
     const url = 'https://yt-music-api.herokuapp.com/api/yt/song/'
-
     const [song, setSong] = useState([])
-
     const { videoId } = useParams()
+    const [player, setPlayer] = useState()
 
     useEffect(() => {
+        loadPlayer()
         fetch(url + videoId)
             .then(res => res.json())
             .then(data => setSong({
-                videoId : data.videoId,
-                type : data.type,
-                name : data.name,
-                artist : data.artist.name,
-                album : data.album.name,
-                img : data.thumbnails[0].url,
-                imgW : data.thumbnails[0].width,
-                imgH : data.thumbnails[0].height
-                })
+                videoId: data.videoId,
+                type: data.type,
+                name: data.name,
+                artist: data.artist.name,
+                album: data.album.name,
+                img: data.thumbnails[0].url,
+                imgW: data.thumbnails[0].width,
+                imgH: data.thumbnails[0].height
+
+            })
             )
-    })
+    }, [])
+
+    function loadPlayer() {
+        let ytPlayer = new YT.Player('yt-player', {
+            height: '0',
+            width: '0',
+            events: {
+                'onStateChange': onPlayerStateChange
+            }
+        });
+        setPlayer(ytPlayer)
+    }
+
+    function playSong() {
+        player.loadVideoById(videoId);
+    }
+    function pauseSong() {
+        player.pauseVideo();
+    }
+    function resumeSong() {
+        player.playVideo();
+    }
+
+    function onPlayerStateChange(event) {
+        if (event.data != YT.PlayerState.PLAYING) return
+    }
 
     return (
         <div className="playingPage">
             <h3>videoId: {videoId}</h3>
-            <div> <img className="albumimg" src={song.img} width="200px" height="200px" alt="img"/> </div>
-
+            <div> <img className="albumimg" src={song.img} width="200px" height="200px" alt="img" /> </div>
             <h2>{song.name}</h2>
             <h3>{song.artist}</h3>
             <h3 className="albumText">{song.album}</h3>
+            <div id="yt-player" ></div>
+            <button onClick={playSong}> play</button>
+            <button onClick={pauseSong}> pause</button>
+            <button onClick={resumeSong}>Resume</button>
         </div>
     )
 }
