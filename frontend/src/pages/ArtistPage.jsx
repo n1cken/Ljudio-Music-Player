d84a0d9dfa6275ece962b8bfa2dd112bb9b976d1
nicholas <nicken333@gmail.com>
2021-10-12 00:00:02 +0200

    render image and description on artist page

--- frontend/src/pages/ArtistPage.jsx
+++ frontend/src/pages/ArtistPage.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
+import '../styles/ArtistPage.css'
 
 function ArtistPage() {
 
@@ -9,25 +10,32 @@ function ArtistPage() {
 
     const [artist, setArtist] = useState([])
 
+
     useEffect(() => {
         fetch(urlArtist + artistId)
             .then(res => res.json())
             .then(data => setArtist( {
                 name : data.name,
                 description : data.description,
-                songs : data.products.songs.content,
+                img : data.thumbnails[0].url,
+                songs : data.products.songs,
                 }
             ))
 
+
     })
+
 
     return (
 
         <div>
-            <div className="songHeader">Artists</div>
-                <div> {artist.name} </div>
-                <div> {artist.description} </div>
+            <div className="artistPageHead">
+                <img className="artistPageImg" src={artist.img} alt="image"/>
+                <div className="artistPageName"> {artist.name} </div>
+            </div>
+                <div className="artistPageDesc"> {artist.description} </div>
         </div>
+
     );
 }
 
