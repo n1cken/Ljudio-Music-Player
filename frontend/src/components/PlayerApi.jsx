10b27d29b8aba237afdb07e9f64326f1a89e8a99
dennishoang2010 <61258188+dennishoang2010@users.noreply.github.com>
2021-10-13 16:48:50 +0200

    added Slider
    slider shows current time also able to manipulate the song time by clicking or dragging (NOTE: need to update the setInterval to pause when user is trying to change time)

--- frontend/src/components/PlayerApi.jsx
+++ frontend/src/components/PlayerApi.jsx
@@ -15,11 +15,39 @@ function PlayerApi() {
   const url = 'https://yt-music-api.herokuapp.com/api/yt/song/'
   const videoId = context
   const [player, setPlayer] = useState()
+  const [progress, setProgress] = useState(0)
+
+
 
   useEffect(() => {
     loadPlayer()
+    context.videoId
     fetch(url + videoId)
+    console.log(context.player)
   }, [])
+
+  useEffect(() => {
+    setInterval(() => {
+      if (!context.player) return
+
+      let currentTime = context.player.getCurrentTime()
+      let duration = context.player.getDuration()
+      let playedPercent = (currentTime / duration) * 100
+
+
+      setProgress(playedPercent)
+
+    }, 100);
+  }, [context.player])
+
+
+
+  function changeSongPosition(e) {
+    setProgress(e.target.value)
+
+    let newPosition = context.player.getDuration() / e.target.value
+    context.player.seekTo(newPosition, true)
+  }
 
   function loadPlayer() {
     let ytPlayer = new YT.Player('yt-player', {
@@ -39,17 +67,20 @@ function PlayerApi() {
   function playSong() {
     console.log(context)
     player.loadVideoById(context.videoId);
+
   }
   function pauseSong() {
     player.pauseVideo();
   }
   function resumeSong() {
     player.playVideo();
+
   }
 
   function onPlayerStateChange(event) {
     if (event.data != YT.PlayerState.PLAYING) return
   }
+
 
 
 
@@ -59,9 +90,10 @@ function PlayerApi() {
       <div className="artistText">{context.artist} </div>
       <div id="yt-player"></div>
       <input type="range"
-        min="1"
-        max="100"
+        value={progress}
+        onChange={changeSongPosition}
         className="slider"
+        style={{ width: '90%' }}
         id="myRange"
       />
       <div className="controllers">
